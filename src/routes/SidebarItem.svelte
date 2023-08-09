e81606b5786a901b9d4ba176338c2db811e557e3
John Qian <johnlongqian@gmail.com>
2023-08-09 02:14:22 -0700

    support links, small fixes

--- src/routes/SidebarItem.svelte
+++ src/routes/SidebarItem.svelte
@@ -8,4 +8,4 @@
     console.log("TO", to);
 </script>
 
-<a href={to} class={classNames("py-1 px-4 opacity-60 hover:opacity-100 text-sm", {"opacity-100": activeUrl === to})}><slot/></a>
+<a href={to} class={classNames("py-1 px-4 hover:opacity-100 text-sm", {"opacity-100": activeUrl === to, "opacity-60": activeUrl !== to})}><slot/></a>
