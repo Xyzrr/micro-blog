c3c183284d839ebcd61d644617b1c827e8a12d94
John Qian <johnlongqian@gmail.com>
2023-08-09 13:54:39 -0700

    ai safety

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -6,6 +6,7 @@
     <div class="list-none flex flex-col pt-10 sticky top-0">
         <SidebarItem to="/">Home</SidebarItem>
         <SidebarItem to="/startup-spark">How a startup loses its spark</SidebarItem>
+        <SidebarItem to="/ai-safety">My take on AI safety</SidebarItem>
         <SidebarItem to="/rnns">Why RNNs work</SidebarItem>
         <SidebarItem to="/sf-paradox">SF's paradox</SidebarItem>
     </div>
