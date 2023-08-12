576b5af67be2bf387d8eeec9a35ff5e4eeb356dc
John Qian <johnlongqian@gmail.com>
2023-08-12 13:03:53 -0700

    google analytics

--- src/routes/SidebarItem.svelte
+++ src/routes/SidebarItem.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-    import { page } from '$app/stores';
-    import classNames from 'classnames';
+	import { page } from '$app/stores';
+	import classNames from 'classnames';
 
 	export let to: string;
-    $: activeUrl = $page.url.pathname;
-
-    console.log("TO", to);
+	$: activeUrl = $page.url.pathname;
 </script>
 
-<a href={to} class={classNames("py-1 px-4 hover:opacity-100 text-sm", {"opacity-100": activeUrl === to, "opacity-60": activeUrl !== to})}><slot/></a>
+<a
+	href={to}
+	class={classNames('py-1 px-4 hover:opacity-100 text-sm', {
+		'opacity-100': activeUrl === to,
+		'opacity-60': activeUrl !== to
+	})}><slot /></a
+>
