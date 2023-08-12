8030fbee8f280d9998a01947d870efcffe2408b1
John Qian <johnlongqian@gmail.com>
2023-08-12 12:16:46 -0700

    email

--- src/routes/(posts)/+page.svelte
+++ src/routes/(posts)/+page.svelte
@@ -3,3 +3,8 @@
 	Thanks for visiting my blog. I'm currently an engineer at Adept. My thoughts don't reflect my
 	workplace. Here I try to distill my thoughts to their simplest forms.
 </p>
+<p>
+	If my blog posts resonate with you, feel free to reach out to me at <a
+		href="mailto:johnlongqian+blog@gmail.com">johnlongqian+blog@gmail.com</a
+	>
+</p>
