3b4ed754fcb91ecf1e307103e34acd9e41584ba7
John Qian <johnlongqian@gmail.com>
2023-08-13 21:58:05 -0700

    bring back roast

---
+++ src/routes/(posts)/roast/+page.svelte
@@ -0,0 +1,63 @@
+<h1>We don't roast anymore</h1>
+<div class="opacity-60 mb-8">July 2021</div>
+<p>
+	In high school, kids were brutally honest. Whenever I did something foolish, other kids would make
+	sure I knew. Taken to the extreme, this becomes bullying. But I think the level of criticism I
+	received in high school was net beneficial for me. Not all of the feedback was worth following,
+	but it was always useful to know what people honestly thought.
+</p>
+<p>
+	As I’ve gotten older, I’ve noticed that criticisms have gotten rarer. In college, flattery became
+	more common than criticism. Post-college, it&#39;s become unusual to hear any form of negativity
+	cast upon someone in the same room. Giving unsolicited feedback has almost become a sign of social
+	ineptitude.
+</p>
+<p>
+	I think there’s many reasons for this, but the main one is that as you get older, you become less
+	“stuck” to people [1]. Most kids in high school have a few friends that they do everything
+	together with, and they know they&#39;ll be together for 4 years. So when someone in the group has
+	a character flaw, it&#39;s easier to make them fix it than to ignore it. But as you get older, you
+	develop multiple disparate friend groups, and forced social interactions become more rare. So when
+	one of your friends has a character flaw, you can opt to avoid doing activities with them where
+	that flaw poses a problem. You can just enjoy the good parts of each friend you have.
+</p>
+<p>
+	I think this is an under-appreciated reason that adults tend to calcify their ideas and habits. No
+	one tells them when they’re wrong because it’s not worth it [2].
+</p>
+<p>
+	So if you want to keep improving like you did in high school, what can you do about this? I think
+	it&#39;s a hard problem. Explicitly asking your friends for feedback may help, but not
+	necessarily. I once overheard an executive say, &quot;when someone asks me for honest criticism, I
+	almost never actually give it to them. Unless they&#39;re my employee, there&#39;s just no value
+	in it for me.&quot; So is the solution to ask your manager for feedback? Probably not; it&#39;s
+	never worked for me. As long as you do your job fine, your manager doesn&#39;t have much incentive
+	to give you criticism either.
+</p>
+<p>I can think of only two partial solutions.</p>
+<p>
+	The first is to improve at reading people. This is expected and obligatory, and may actually be
+	another reason why adults criticize less than children. They expect you to notice increasingly
+	subtle hints, so that they never have to say anything directly.
+</p>
+<p>
+	The other is to find a committed romantic partner. This creates a situation similar to the high
+	school friend group. Your partner has to deal with your flaws every day, and may even be judged
+	for them. So they&#39;re incentivized to actually fix you.
+</p>
+<p>
+	I don&#39;t find either of these solutions really satisfying. Sometimes I wish people would
+	continue to spout their thoughts unfiltered like they did in high school. Fortunately by being
+	aware of the problem, you can avoid calcifying too early.
+</p>
+<p class="pt-16">
+	[1] I originally thought that it’s because it takes time to realize that fixing people isn’t worth
+	it. I no longer think this is the reason; in high school, fixing people often is worth it. Kids
+	are quick at learning what behaviors work; what takes time is unlearning behaviors that have
+	stopped working.
+</p>
+<p>
+	[2] A surprising implication of this is that giving someone criticism is a powerful statement of
+	loyalty. It means you intend on spending enough time with them for them to be worth fixing. Of
+	course, this positive message may not get through.
+</p>
--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -1,26 +1,27 @@
 <script>
-	import SidebarItem from "./SidebarItem.svelte";
+	import SidebarItem from './SidebarItem.svelte';
 </script>
 
 <div class="w-60 shrink-0 h-full">
-    <div class="list-none flex flex-col pt-10 sticky top-0">
-        <SidebarItem to="/">Home</SidebarItem>
-        <SidebarItem to="/startup-spark">How a startup loses its spark</SidebarItem>
-        <SidebarItem to="/ai-safety">My take on AI safety</SidebarItem>
-        <SidebarItem to="/simplicity">Simplicity scarcity</SidebarItem>
-        <SidebarItem to="/rnns">Why RNNs work</SidebarItem>
-        <SidebarItem to="/sf-paradox">SF's paradox</SidebarItem>
-    </div>
+	<div class="list-none flex flex-col pt-10 sticky top-0">
+		<SidebarItem to="/">Home</SidebarItem>
+		<SidebarItem to="/startup-spark">How a startup loses its spark</SidebarItem>
+		<SidebarItem to="/ai-safety">My take on AI safety</SidebarItem>
+		<SidebarItem to="/simplicity">Simplicity scarcity</SidebarItem>
+		<SidebarItem to="/rnns">Why RNNs work</SidebarItem>
+		<SidebarItem to="/roast">We don't roast anymore</SidebarItem>
+		<SidebarItem to="/sf-paradox">SF's paradox</SidebarItem>
+	</div>
 </div>
 
 <style>
-    a {
-        padding: 4px 16px;
-        opacity: 60%;
-        font-size: 14px;
-        text-decoration: none;
-    }
-    a:hover {
-        opacity: 100%;
-    }
+	a {
+		padding: 4px 16px;
+		opacity: 60%;
+		font-size: 14px;
+		text-decoration: none;
+	}
+	a:hover {
+		opacity: 100%;
+	}
 </style>
