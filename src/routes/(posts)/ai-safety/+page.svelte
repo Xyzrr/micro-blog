c343ba52f9eeb48323b8fc1d7796d47d701c28e7
John Qian <johnlongqian@gmail.com>
2023-08-29 23:07:16 -0700

    instrumental convergence

--- src/routes/(posts)/ai-safety/+page.svelte
+++ src/routes/(posts)/ai-safety/+page.svelte
@@ -15,24 +15,24 @@
 	much about making money rather than simply doing what they enjoy.
 </p>
 <p>
-	Researchers are primarily concerned with existential risk. The way I’ve mostly seen it presented
-	is that it’s impossible to align a superintelligent AI, so it will do whatever it wants, which
-	more likely than not will end up killing us. While this seems plausible to me, I haven’t found an
-	argument for it that I understood. It seems to me that if you took a good, empathetic human and
-	made them a billion times more intelligent, they would still avoid hurting humans. After all,
-	we’re able to feel abundant empathy for the mentally disabled. It’s unclear to me why a
-	superintelligent AI trained by humans will have different values than a superintelligent human. Is
-	the idea that the superintelligent human would find easier ways to satisfy their reward systems,
-	like by sticking electrodes into their brains?
+	Researchers are primarily concerned with existential risk. The framing I've seen most often is
+	that it may be impossible to align superintelligence, so whatever goal it ends up with will likely
+	involve killing us as a subgoal due to [instrumental
+	convergence](https://en.wikipedia.org/wiki/Instrumental_convergence). While this seems plausible
+	to me, I haven’t found an argument for it that I understood. It seems to me that if you took a
+	good, empathetic human and made them a billion times more intelligent, they would still avoid
+	hurting humans. After all, we're able to feel abundant empathy towards the mentally disabled; we
+	don't kill them when they get in the way of our goals. It’s unclear to me why a superintelligent
+	AI trained by humans will have different values than a superintelligent human.
 </p>
 <p>
 	Regardless, I think existential risk will come long before AI is smart enough to unalign itself
-	with its developers, because some developers will be unaligned. We’ve already had close calls with
-	nuclear weapons and synthetic viruses. I think the reason those weapons haven’t ended the world
-	yet is because they’re hard to use tactically—it’s hard to get them to accomplish what a group of
-	powerful people actually want. Not many people want a city reduced to rubble, and we can’t control
-	what a virus is going to mutate into as it spreads. AI is considerably more useful. You can use it
-	to manipulate and subjugate precisely the subset of society you want to target.
+	with its developers, because some developers themselves will be unaligned. We’ve already had close
+	calls with nuclear weapons and synthetic viruses. I think the reason those weapons haven’t ended
+	the world yet is because they’re hard to use tactically—it’s hard to get them to accomplish what a
+	group of powerful people actually want. Not many people want a city reduced to rubble, and we
+	can’t control what a virus is going to mutate into as it spreads. AI is considerably more useful.
+	You can use it to manipulate and subjugate precisely the subset of society you want to target.
 </p>
 <p>
 	There will almost certainly be more good people using AI than bad people using AI, so they’ll be
