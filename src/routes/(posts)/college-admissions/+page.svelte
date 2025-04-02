2b731e695667265d979c9ef1214a8420ddf4c2ad
John Qian <johnqian@Johns-MacBook-Pro-2.local>
2025-04-02 13:39:35 -0700

    to the rejected post

---
+++ src/routes/(posts)/college-admissions/+page.svelte
@@ -0,0 +1,50 @@
+<svelte:head>
+	<title>To the rejected</title>
+</svelte:head>
+
+<h1>To the rejected</h1>
+<div class="opacity-60 mb-8">April 2024</div>
+
+<p>
+	College admissions decisions are rolling out now. It seems they've grown even less reasonable.
+</p>
+
+<p>
+	I'm writing this for anyone who just got rejected from top colleges because they focused on coding
+	cool projects in school rather than mix-maxing admissions.
+</p>
+
+<p>
+	A lot of people will tell you your grief will seem silly in retrospect, but frankly it never did
+	for me. The problem with a lackluster pedigree is that you have to convince people you're worth
+	knowing, rather than them just assuming you are. This is okay if you're comfortable with a quiet
+	big tech job, because that only requires a few engineers to recognize your talent. But if you want
+	a career that involves interacting with a lot of people, this kind of thing slows down your
+	exponential growth, which is a big deal.
+</p>
+
+<p>
+	You can compensate by grabbing some other prestigious badge, like by joining a high profile tech
+	company, which engineers and recruiters will weigh far more than college. But in truth nothing has
+	quite the cross-industry, cross-age recognition that universities do, which matters if you need to
+	e.g. sell services to execs at non-tech businesses. Being underestimated by people outside the
+	tech bubble also changes your general social experience of life; you inadvertently become more
+	married to tech.
+</p>
+
+<p>
+	I don’t know if there existed any “silver lining” to rejection that could have made me feel
+	better. The best one was perhaps the idea that the difficulty mode of your life is now set, and if
+	you’re truly a god gamer then you’ll embrace the higher difficulties. And there will be people who
+	respect you more for it.
+</p>
+
+<p>
+	So what now? If you don’t intend on lowering your ambitions, in order to still achieve them you’ll
+	need to multiply your risk tolerance and expose yourself to opportunities to get lucky. There are
+	a lot more such opportunities in SF than most would think (I’ve personally just-missed several
+	that would have made me pretty rich, I expect to catch one shortly). I recommend visiting / moving
+	here as soon as you can, meeting people at hackathons, building in public, and engaging with the
+	internet. This is all really hard if you're introverted, which held me back, but you have time to
+	find your rhythm. I’m down to hop on a call with anyone who needs it, just reach out here.
+</p>
--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -5,6 +5,7 @@
 <div class="w-60 shrink-0 h-full">
 	<div class="list-none flex flex-col pt-10 sticky top-0">
 		<SidebarItem to="/">Home</SidebarItem>
+		<SidebarItem to="/college-admissions">To the rejected</SidebarItem>
 		<SidebarItem to="/startup-spark">How a startup loses its spark</SidebarItem>
 		<SidebarItem to="/ai-safety">My take on AI safety</SidebarItem>
 		<SidebarItem to="/simplicity">Simplicity scarcity</SidebarItem>
