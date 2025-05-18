77e756f30baf0d22628dce06b2df981d049de6d8
John Qian <johnqian@Johns-MacBook-Pro-2.local>
2025-05-17 23:28:57 -0700

    more content

--- src/routes/(posts)/what-people-think/+page.svelte
+++ src/routes/(posts)/what-people-think/+page.svelte
@@ -36,15 +36,24 @@
 	social capital suddenly becomes one of the main bottlenecks to your company's success. Winning the
 	gratitude/trust/respect of someone is a lot like buying a call option on them. There's theta decay
 	as they gradually forget about you, but if they become successful down the road--especially if
-	it's partly because of you--you can 'exercise' your option for a lot of value.
+	it's partly because of you--they may come back and bring you a lot of value.
 </p>
 
 <p>
-	This is why Sam Altman, possibly the greatest social capital accumulator to ever live, was known
-	for taking cold calls from 15 year old kids. Before he became too famous, this was a very good
-	filter; if a kid knew who Sam Altman was and had the agency to cold call him, that kid was
-	probably going to become fairly successful. The most SPY ever grew in a year is 31.3%. A
-	hyper-ambitious kid may grow that much every 6 months. That's a good investment.
+	This is why Sam Altman, possibly the greatest social capital accumulator to ever live, despite
+	being cold to anyone he considered ineffective, was known for taking cold calls from 15 year old
+	kids. Before he became too famous, this was a very good filter; if a kid knew who Sam Altman was
+	and had the agency to cold call him, that kid was probably going to become fairly successful. The
+	most the S&P 500 ever grew in a year is 31.3%. A hyper-ambitious kid may grow that much every 6
+	months. That's a good investment.
+</p>
+
+<p>
+	Side story: Sam has gone further than this. In 2016 at WWDC, I saw him getting into his car to go
+	home to his family. One of the high school scholars ran up to him and asked to join him. He
+	agreed, and the kid got to meet his family. They chatted until Sam had to shoo him out because
+	Elon was coming over to discuss some stuff about a research lab. That kid raised a series A last
+	year.
 </p>
 
 <p>
@@ -52,27 +61,54 @@
 	epistemic, not prescriptive. I suspect that thinking this way all the time isn't healthy, and I
 	think it's very much worth helping people who will never be able to help you back. But I suspect
 	that the ability and tendency to analyze social interactions as investments is the single most
-	consistent outlier trait of the wealthiest people in the world.
+	consistent outlier trait of the wealthiest people in the world. If you've ever wondered how some
+	people seem to always end up at the top despite not having clearly outstanding talents, this is
+	probably the reason. It's actually a lot simpler than the 4D chess people think billionaires play.
 </p>
 
 <p>
-	I've discussed gaining social capital by helping individual people. But these days, it seems like
-	most social capital is actually acquired by creating value for people in bulk or by working on
-	your public image, managing how people perceive you from afar.
+	I've discussed gaining social capital by helping individual people. But these days when everyone's
+	chronically online, it seems like most social capital is actually acquired in the form of social
+	status, which is basically the side of social capital that extends to people you've never met. You
+	can acquire social status in two ways:
+</p>
+
+<ol>
+	<li>1. Quietly, by managing the impression you leave to inbounds</li>
+	<li>2. Loudly, by thrusting yourself outbound into the public conscience</li>
+</ol>
+
+<p>
+	I find that in tech, the majority of #1 for most people is just their LinkedIn profiles. It's
+	typically the first thing people look up when interacting with you. Probably second to your
+	LinkedIn profile is your physical appearance. "Hot and smart" is the highest status you can
+	achieve without actually _doing_ anything, and your looks + LinkedIn communicates that very
+	efficiently.
 </p>
 
 <p>
-	At this point it's probably helpful to define social capital in a more quantifiable way. Your
-	social capital is the sum of all the help you could get from other people, minus the effort it'd
-	take to get that help. It's hard to reduce this to one number because these things are so illiquid
-	and nonfungible, but you can get an upper bound on the value of anything by imagining how much
-	you'd sell it for if you could. Would you rather lose half your net worth or half your social
-	capital? If you answered the former, that means you have more wealth stored in net worth than
-	social capital, which is a good thing if you're old, but is probably suboptimal if you're young.
+	To achieve outlier level status, you need to either put yourself out there or build a product, put
+	the product out there, and acquire status by proxy of the product. The former is more common in
+	LA, the latter is more common in SF, though even SF is starting to lean more into the former.
 </p>
 
 <p>
-	Having an impressive public image on the internet nets you a lot of social capital, because it
-	increases almost anyone's willingness to help you. Being physically attractive has similar
-	effects, though capitalizing on that comes with some perils.
+	Status is inherently compounding. When you have enough status, people find excuses to introduce
+	you to their friends, rather than finding excuses not to. The higher your status, the more they
+	filter their introductions to other high status people.
+</p>
+
+<p>
+	At this point it's helpful to define social capital in a quantifiable way. Your social capital is
+	the sum of all the help you could get from other people, minus the effort it'd take to get that
+	help. It's hard to reduce this to one number because these things are so illiquid and nonfungible,
+	but you can get an upper bound on the value of anything by imagining how much you'd sell it for if
+	you could. Would you rather lose half your net worth or half your social capital? If you answered
+	the former, that means you have more wealth stored in net worth than social capital, which is a
+	good thing if you're old, but is probably suboptimal if you're young.
+</p>
+
+<p>
+	When you think about your total wealth as net worth + social capital (+ years left of good health,
+	but let's not think about that),
 </p>
